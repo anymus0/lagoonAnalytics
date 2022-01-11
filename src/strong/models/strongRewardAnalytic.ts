59c65adb72520c04521a855856b3d7caff8a7828
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-01-11 16:01:23 +0100

    add strong ABIs, schema and model

--- src/strong/models/strongRewardAnalytic.ts
+++ src/strong/models/strongRewardAnalytic.ts
@@ -1,4 +1,7 @@
+import mongoose from 'mongoose';
+
 export interface strongRewardAnalytics {
+  _id: typeof mongoose.Schema.Types.ObjectId;
   date: Date;
   marketPriceinUSD: number;
   unclaimedRewards: number;
---
+++ src/strong/schemas/strongRewardAnalyticsSchema.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { strongRewardAnalytics } from "./../models/strongRewardAnalytic";
+
+const strongRewardAnalyticsSchema: mongoose.Schema<strongRewardAnalytics> = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  date: { type: Date, required: true },
+  marketPriceinUSD: { type: Number, required: true },
+  unclaimedRewards: { type: Number, required: true },
+  claimedRewards: { type: Number, required: true },
+  claimedRewardsInUSD: { type: Number, required: true },
+  allRewards: { type: Number, required: true },
+  allRewardsInUSD: { type: Number, required: true },
+  numOfNodes: { type: Number, required: true },
+  serviceV1NumOfNodes: { type: Number, required: true },
+  serviceV1UnclaimedRewards: { type: Number, required: true },
+  serviceV2NumOfNodes: { type: Number, required: true },
+  serviceV2UnclaimedRewards: { type: Number, required: true },
+});
+
+export const StrongRewardAnalytics: mongoose.Model<strongRewardAnalytics> =
+  mongoose.model("StrongRewardAnalytics", strongRewardAnalyticsSchema);
