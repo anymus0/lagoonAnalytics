72e45a4b7e5f7bbf902802f6812ef576fd4c1b20
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-01-11 22:42:14 +0100

    add missing .js extensions in imports

--- src/EthWallet/routes/index.ts
+++ src/EthWallet/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response } from "express";
 // middlewares
-import { validateEthWallet } from './../../global/middlewares/validateEthWallet';
-import { apiKeyAuth } from './../../global/middlewares/apiKeyAuth'
+import { validateEthWallet } from './../../global/middlewares/validateEthWallet.js';
+import { apiKeyAuth } from './../../global/middlewares/apiKeyAuth.js'
 // controllers
 import { getEthWallet } from "./../controllers/getEthWallets.js";
-import { addEthWallet } from "./../controllers/addEthWallet";
-import { deleteEthWallet } from "./../controllers/deleteEthWallet";
+import { addEthWallet } from "./../controllers/addEthWallet.js";
+import { deleteEthWallet } from "./../controllers/deleteEthWallet.js";
 
 const router = Router();
 
