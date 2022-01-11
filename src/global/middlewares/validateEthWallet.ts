b063cbd1d49833f31c71510d4dd12560dbadbb18
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-01-11 22:04:06 +0100

    create 'validateEthWallet' global middleware

---
+++ src/global/middlewares/validateEthWallet.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+
+const validateEthWallet = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (req.headers.ethWallet.slice(0, 2) === "0x") {
+      next();
+    } else {
+      res
+        .status(418)
+        .json({
+          success: false,
+          message: "Eth wallet format is invalid!",
+          details: `An eth wallet address must start with "0x"`,
+          result: null,
+        })
+        .end();
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Error in validateEthWallet middleware!",
+        details: error,
+        result: null,
+      })
+      .end();
+    return false;
+  }
+};
