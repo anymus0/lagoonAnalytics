bf4acfb096dd6a17982820cc763500a7d3f185b8
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-01-15 00:12:10 +0100

    add 'strong/addStrongAnalytics' route

--- src/strong/routes/index.ts
+++ src/strong/routes/index.ts
@@ -1,14 +1,22 @@
 import { Router, Request, Response } from "express";
 import { getAllStrongAnalytics } from "./../controllers/getAllStrongAnalytics.js";
 import { getStrongAnalyticsByDate } from "./../controllers/getStrongAnalyticsByDate.js";
+import { apiKeyAuth } from './../../global/middlewares/apiKeyAuth.js'
 
 const router = Router();
 
+// GET /strong/getAllStrongAnalytics
 router.get("/getAllStrongAnalytics", (req: Request, res: Response) => {
   getAllStrongAnalytics(req, res);
 });
 
+// GET /strong/getStrongAnalyticsByDate
 router.get("/getStrongAnalyticsByDate", (req: Request, res: Response) => {
+  getStrongAnalyticsByDate(req, res);
+});
+
+// POST /strong/addStrongAnalytics
+router.post("/addStrongAnalytics", apiKeyAuth, (req: Request, res: Response) => {
   getStrongAnalyticsByDate(req, res);
 });
 
