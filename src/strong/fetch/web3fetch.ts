9b9584c087c847605399bf67617fbb969e580122
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-01-15 15:06:39 +0100

    add NODE_RPC_URL env variable

--- src/strong/fetch/web3fetch.ts
+++ src/strong/fetch/web3fetch.ts
@@ -1,7 +1,9 @@
 import { BigNumber, ethers } from "ethers";
-import { ERC20Abi } from "../../global/models/ERC20Abi";
-import { strongServiceV1Abi } from "../models/strongServiceV1Abi";
-import { strongServiceV2Abi } from "../models/strongServiceV2Abi";
+import { ERC20Abi } from "../../global/models/ERC20Abi.js";
+import { strongServiceV1Abi } from "../models/strongServiceV1Abi.js";
+import { strongServiceV2Abi } from "../models/strongServiceV2Abi.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const nodeRpcUrl = process.env.NODE_RPC_URL;
 const web3Provider = new ethers.providers.JsonRpcProvider(nodeRpcUrl);
@@ -33,9 +35,11 @@ const strongServiceV2Contract = new ethers.Contract(
 
 export const getStrongBalanceOfWallet = async (walletAddress: string) => {
   try {
-    const balanceInWei: BigNumber = await strongTokenContract.balanceOf(walletAddress);
+    const balanceInWei: BigNumber = await strongTokenContract.balanceOf(
+      walletAddress
+    );
     const balance: number = Number.parseFloat(
-      ethers.utils.formatUnits(balanceInWei, "wei")
+      ethers.utils.formatUnits(balanceInWei, 18)
     );
     return balance;
   } catch (error) {
@@ -50,7 +54,7 @@ export const getServiceV1UnclaimedRewards = async (walletAddress: string) => {
     const unclaimedRewardsFromV1InWei: BigNumber =
       await strongServiceV1Contract.getRewardAll(walletAddress, currentBlock);
     const unclaimedRewardsFromV1 = Number.parseFloat(
-      ethers.utils.formatUnits(unclaimedRewardsFromV1InWei, "wei")
+      ethers.utils.formatUnits(unclaimedRewardsFromV1InWei, 18)
     );
     return unclaimedRewardsFromV1;
   } catch (error) {
@@ -61,17 +65,17 @@ export const getServiceV1UnclaimedRewards = async (walletAddress: string) => {
 
 export const getServiceV2UnclaimedRewards = async (walletAddress: string) => {
   try {
-    const entityNodeCount: number =
-      await strongServiceV2Contract.entityNodeCount(walletAddress);
-    let unclaimedRewardsFromV2InWei = 0;
-    for (let nodeId = 1; nodeId <= entityNodeCount; nodeId++) {
-      const rewardOfNodeIdInWei: number =
-        await strongServiceV2Contract.getNodeReward(walletAddress, nodeId);
-      unclaimedRewardsFromV2InWei += rewardOfNodeIdInWei;
-    }
-    const unclaimedRewardsFromV2 = Number.parseFloat(
-      ethers.utils.formatUnits(unclaimedRewardsFromV2InWei, "wei")
+    const entityNodeCount: number = Number.parseInt(
+      await strongServiceV2Contract.entityNodeCount(walletAddress)
     );
+    let unclaimedRewardsFromV2 = 0;
+    for (let nodeId = 1; nodeId <= entityNodeCount; nodeId++) {
+      console.log(nodeId);
+      const rewardOfNodeIdInWei: BigNumber = await strongServiceV2Contract.getNodeReward(walletAddress, nodeId);
+      const rewardOfNodeId: number = Number.parseFloat(ethers.utils.formatUnits(rewardOfNodeIdInWei, 18));
+      unclaimedRewardsFromV2 += rewardOfNodeId;
+      console.log(`Rew of node#${nodeId}: ${rewardOfNodeIdInWei}`);
+    }
     return unclaimedRewardsFromV2;
   } catch (error) {
     console.error(error);
@@ -81,10 +85,9 @@ export const getServiceV2UnclaimedRewards = async (walletAddress: string) => {
 
 export const getServiceV1NumOfNodes = async (walletAddress: string) => {
   try {
-    const entityNodeCount: number = await strongServiceV1Contract.entityNodeCount(
-      walletAddress
-    );
-    return entityNodeCount;
+    const entityNodeCount: BigNumber =
+      await strongServiceV1Contract.entityNodeCount(walletAddress);
+    return entityNodeCount.toNumber();
   } catch (error) {
     console.error(error);
     return 0;
@@ -93,9 +96,9 @@ export const getServiceV1NumOfNodes = async (walletAddress: string) => {
 
 export const getServiceV2NumOfNodes = async (walletAddress: string) => {
   try {
-    const entityNodeCount: number =
+    const entityNodeCount: BigNumber =
       await strongServiceV2Contract.entityNodeCount(walletAddress);
-    return entityNodeCount;
+    return entityNodeCount.toNumber();
   } catch (error) {
     console.error(error);
     return 0;
